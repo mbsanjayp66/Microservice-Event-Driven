b543330e802fb1213902523babe51717c5811db1
mbsanjayp66 <mbsanjayp66@gmail.com>
2024-03-10 12:56:09 +0530

    Initial Commit

---
+++ Twitter4J/src/main/java/com/sanjay/microservice/Twitter4JApplication.java
@@ -0,0 +1,13 @@
+package com.sanjay.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Twitter4JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Twitter4JApplication.class, args);
+	}
+
+}
---
+++ Twitter4J/src/test/java/com/sanjay/microservice/Twitter4JApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjay.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Twitter4JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
